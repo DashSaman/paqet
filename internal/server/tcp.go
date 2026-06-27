bb28e69801dd35395dc48192a131096314df834e
hanselime <253621328+hanselime@users.noreply.github.com>
2026-06-27 13:26:37 +0330

    internal/server: lower tcp dial timeout

--- internal/server/tcp.go
+++ internal/server/tcp.go
@@ -3,11 +3,12 @@ package server
 import (
 	"context"
 	"net"
+	"time"
+
 	"paqet/internal/flog"
 	"paqet/internal/pkg/buffer"
 	"paqet/internal/protocol"
 	"paqet/internal/tnet"
-	"time"
 )
 
 func (s *Server) handleTCPProtocol(ctx context.Context, strm tnet.Strm, p *protocol.Proto) {
@@ -16,7 +17,7 @@ func (s *Server) handleTCPProtocol(ctx context.Context, strm tnet.Strm, p *proto
 }
 
 func (s *Server) handleTCP(ctx context.Context, strm tnet.Strm, addr string) {
-	dialer := &net.Dialer{Timeout: 10 * time.Second}
+	dialer := &net.Dialer{Timeout: 8 * time.Second}
 	conn, err := dialer.DialContext(ctx, "tcp", addr)
 	if err != nil {
 		flog.Errorf("failed to establish TCP connection to %s for stream %d: %v", addr, strm.SID(), err)
