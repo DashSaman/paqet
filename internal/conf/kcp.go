0851befa73d043c7f9ded88839e31d0b4d923b1a
Saman Kalantari <140158372+DashSaman@users.noreply.github.com>
2026-08-16 17:40:58 +0330

    feat(metrics): add optional KCP stats interval

--- internal/conf/kcp.go
+++ internal/conf/kcp.go
@@ -40,9 +40,11 @@ type KCP struct {
 
 	Smuxkalive_   int `yaml:"smuxkalive"`
 	Smuxktimeout_ int `yaml:"smuxktimeout"`
+	StatsInterval_ int `yaml:"stats_interval"`
 
 	Smuxkalive   time.Duration  `yaml:"-"`
 	Smuxktimeout time.Duration  `yaml:"-"`
+	StatsInterval time.Duration `yaml:"-"`
 	Block        kcp.BlockCrypt `yaml:"-"`
 }
 
@@ -164,9 +166,13 @@ func (k *KCP) validate() []error {
 	if k.Streambuf < 1024 {
 		errors = append(errors, fmt.Errorf("KCP streambuf must be >= 1024 bytes"))
 	}
+	if k.StatsInterval_ < 0 || k.StatsInterval_ > 3600 {
+		errors = append(errors, fmt.Errorf("KCP stats_interval must be between 0-3600 seconds"))
+	}
 
 	k.Smuxkalive = time.Duration(k.Smuxkalive_) * time.Second
 	k.Smuxktimeout = time.Duration(k.Smuxktimeout_) * time.Second
+	k.StatsInterval = time.Duration(k.StatsInterval_) * time.Second
 
 	return errors
 }
